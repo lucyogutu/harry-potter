b396cfc0bad6c048aac2c3416b2cc19380033ddf
lucyogutu <amondilucy192@gmail.com>
2024-02-16 11:45:38 +0300

    feat: project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './character/characters/characters.component';
+import { CharacterComponent } from './character/character/character.component';
+import { CharacterDetailsComponent } from './character/character-details/character-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    CharacterComponent,
+    CharacterDetailsComponent
   ],
   imports: [
     BrowserModule,
