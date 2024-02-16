4ca740d47dddf8f5f780291d3f61c3106bcb02fd
lucyogutu <amondilucy192@gmail.com>
2024-02-16 11:47:53 +0300

    feat: map out a character skeleton

---
+++ src/app/character/character.ts
@@ -0,0 +1,28 @@
+export interface Character {
+  id: string;
+  name: string;
+  alternate_names: string[];
+  species: string;
+  gender: string;
+  house: string;
+  dateOfBirth: string;
+  yearOfBirth: number;
+  wizard: boolean;
+  ancestry: string;
+  eyeColor: string;
+  hairColor: string;
+  wand: Wand;
+  patronus: string;
+  hogwartsStudent: boolean;
+  hogwartsStaff: boolean;
+  actor: string;
+  alternate_actors: string[];
+  alive: boolean;
+  image: string;
+}
+
+export interface Wand {
+  wood: string;
+  core: string;
+  length: number;
+}
