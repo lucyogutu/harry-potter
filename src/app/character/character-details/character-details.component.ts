ce5d48b3e0a83bb18b17b6af56334c21b6d1e75b
lucyogutu <amondilucy192@gmail.com>
2024-02-20 16:43:58 +0300

    feat: unsubscribing from an observable

--- src/app/character/character-details/character-details.component.ts
+++ src/app/character/character-details/character-details.component.ts
@@ -1,25 +1,27 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, OnDestroy, inject } from '@angular/core';
 import { Character } from '../character';
 import { ActivatedRoute } from '@angular/router';
 import { CharacterService } from '../character.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-character-details',
   templateUrl: './character-details.component.html',
   styleUrls: ['./character-details.component.css'],
 })
-export class CharacterDetailsComponent {
+export class CharacterDetailsComponent implements OnDestroy {
   route: ActivatedRoute = inject(ActivatedRoute);
   characterService: CharacterService = inject(CharacterService);
 
   character: Character | undefined;
   date: number = new Date().getFullYear();
   characterId: string = '';
+  paramsObservable;
 
   constructor() {
     // const characterId: string = this.route.snapshot.params['id'];
     // const characterId: string = this.route.snapshot.paramMap.constget('id');
-    this.route.params.subscribe((data) => {
+    this.paramsObservable = this.route.params.subscribe((data) => {
       this.characterId = data['id'];
     });
 
@@ -31,5 +33,9 @@ export class CharacterDetailsComponent {
       .subscribe((character: Character[]) => {
         this.character = character[0];
       });
+  }
+
+  ngOnDestroy() {
+    this.paramsObservable.unsubscribe();
   }
 }
